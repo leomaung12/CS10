df56b7a20c54defaa2cdf73e48bb7616b3ceb35d
1100049812 <1100049812@L815WD220011.edu.cbe.ab.ca>
2022-03-29 14:44:32 -0600

    FromMetoYou

--- FromMeToYou/src/FromMeToYou.java
+++ FromMeToYou/src/FromMeToYou.java
@@ -2,7 +2,11 @@
 public class FromMeToYou {
 
 	public static void main(String[] args) {
-		// Leo
+		// From: Mr. Wilson
+		// Address: Acer Computer, Lab 314
+		// Date: November 2, 2020
+	// 	To: Enter Your First Name and Last Name
+		 // Message: Help! I'm trapped inside a computer!
 
 	}
 
