f4f46169d5cf017692b9cfdcbd94063e69223326
1100049812 <1100049812@L815WD220011.edu.cbe.ab.ca>
2022-03-31 14:43:30 -0600

    Final

--- FromMeToYou/src/FromMeToYou.java
+++ FromMeToYou/src/FromMeToYou.java
@@ -1,16 +1,14 @@
 
-public class FromMeToYou {
+public class FromMeToYou { 
 
 	public static void main(String[] args) {
-		String name = "From: Mr. Wilson\r\n"
-				+ "		Address: Acer Computer, Lab 314\r\n"
-				+ "		 Date: November 2, 2020\r\n"
-				+ "	To: Enter Your First Name and Last Name\r\n"
-				+ "		 Message: Help! I'm trapped inside a computer!";
+		String name = "From: Mr. Wilson"
+				+ "		Address: Acer Computer, Lab 314"
+				+ "		Date: March 31st, 2022"
+				+ "		To: Leo Thein Maung "
+				+ "		Message: Help! I'm trapped inside a computer!";
 		System.out.println(name); 
-		/* 
-		
-
-	}
-
+		}
 }
+
+
