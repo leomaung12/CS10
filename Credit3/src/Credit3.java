66b8c246b47908c48db29c3aaedeaa77100964e6
1100049812 <1100049812@L815WD220011.edu.cbe.ab.ca>
2022-03-31 14:30:34 -0600

    String update

--- Credit3/src/Credit3.java
+++ Credit3/src/Credit3.java
@@ -1,4 +1,4 @@
-public class FromMeToYou
+public class Credit3
 {
 public static void main(String args[])
 {
--- FromMeToYou/src/FromMeToYou.java
+++ FromMeToYou/src/FromMeToYou.java
@@ -2,11 +2,14 @@
 public class FromMeToYou {
 
 	public static void main(String[] args) {
-		// From: Mr. Wilson
-		// Address: Acer Computer, Lab 314
-		// Date: November 2, 2020
-	// 	To: Enter Your First Name and Last Name
-		 // Message: Help! I'm trapped inside a computer!
+		String name = "From: Mr. Wilson\r\n"
+				+ "		Address: Acer Computer, Lab 314\r\n"
+				+ "		 Date: November 2, 2020\r\n"
+				+ "	To: Enter Your First Name and Last Name\r\n"
+				+ "		 Message: Help! I'm trapped inside a computer!";
+		System.out.println(name); 
+		/* 
+		
 
 	}
 
