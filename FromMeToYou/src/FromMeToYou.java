7df4f0afb9fb68bb7870dd5f67cdafb5637c866e
1100049812 <1100049812@L815WD220011.edu.cbe.ab.ca>
2022-04-01 11:06:30 -0600

    Initial

--- FromMeToYou/src/FromMeToYou.java
+++ FromMeToYou/src/FromMeToYou.java
@@ -1,7 +1,8 @@
 
 public class FromMeToYou { 
 
-	public static void main(String[] args) {
+	public static void main(String args []) 
+	{
 		String name = "From: Mr. Wilson"
 				+ "		Address: Acer Computer, Lab 314"
 				+ "		Date: March 31st, 2022"
---
+++ myInitials/src/myInitials/myInitials.java
@@ -0,0 +1,10 @@
+package myInitials;
+
+public class myInitials {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
