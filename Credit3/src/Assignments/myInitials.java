e7b1ff76a7ba46e7173a34ad65ce1ad425de4bd4
1100049812 <1100049812@L815WD220011.edu.cbe.ab.ca>
2022-04-05 14:38:33 -0600

    Final

---
+++ Credit3/src/Assignments/myInitials.java
@@ -0,0 +1,21 @@
+package Assignments;
+
+
+public class myInitials {
+
+	public static void main(String[] args) {
+		String initials = "1"
+				+ "		 |				    ----------                 \r\n"
+				+ "		 |					|			||          | |\r\n"
+				+ "		 |					|			|  |       |  |\r\n"
+				+ "		 |					|			|   |     |   |\r\n"
+				+ "		 |					|			|    |   |    |\r\n"
+				+ "		 |					|			|     | |     |\r\n"
+				+ "		 |					|			|      |      |\r\n"
+				+ "		 -------------		.		|		.		\r\n"
+				+ "		 					";
+
+		 
+		System.out.println(initials);
+	} 
+}
